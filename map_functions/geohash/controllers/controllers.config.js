ec8ab942a6847599309523541f6e03192a043a08
fedIce <freedez1996@gmail.com>
2021-10-27 09:18:41 +0100

    27th Oct 2021

--- map_functions/geohash/controllers/controllers.config.js
+++ map_functions/geohash/controllers/controllers.config.js
@@ -12,32 +12,37 @@ exports.getHash = (req, res) => {
     const data = queryObject.data
     
     const position = geo.point(data.position.lon, data.position.lat);
-    res.status(200).send({name: data.name, position})
+    return res.status(200).send({name: data.name, position})
 }
 
 
 exports.getPlaces = async (req, res) => {
-
-    const queryObject = req.body
-    const data = queryObject.data
-    
-    const firestoreRef = await firestore.collection(data.option)
-    firestoreRef.get().then(snapshot => {
-        return snapshot.forEach(doc => {
-            console.log({ docid: doc.id, pos: doc.data().pos })
-            return { docid: doc.id, pos: doc.data().pos }
+    try{
+        const queryObject = req.body
+        const data = queryObject.data
+        
+        const firestoreRef = await firestore.collection(data.option)
+        firestoreRef.get().then(snapshot => {
+            return snapshot.forEach(doc => {
+                console.log({ docid: doc.id, pos: doc.data().pos })
+                return { docid: doc.id, pos: doc.data().pos }
+            })
         })
-    })
-    const geoRef = await geo.query(firestoreRef).within(geo.point(data.latitude, data.longitude), data.radius, 'pos')
+        const geoRef = await geo.query(firestoreRef).within(geo.point(data.latitude, data.longitude), data.radius, 'pos')
 
-    // const hits = await geofirex.get(geoRef)
-    geoRef.subscribe(v => {
-        res.send({ len: v.length, v })
-    })
+        // const hits = await geofirex.get(geoRef)
+        geoRef.subscribe(v => {
+            return res.send({ len: v.length, v })
+        })
+        // geoRef.unsubscribe();
 
-    // geoRef.unsubscribe();
+    }catch(e) {
+        console.log(e)
+        geoRef.unsubscribe()
+        return res.status(500).send(e)
 
-    // res.status(200).send(hits.catch(err => err))
+    }
+    
 }
 
 
@@ -84,17 +89,29 @@ exports.getFilteredPlaces = async (req, res) => {
     geoRef.subscribe(v => {
 
         if(data.option === "Apartments"){
+
+            v = v.filter(item => item.status ==='available');
+
+
             if(data.min_rent > 50 || data.max_rent < 1000){
                 v = v.filter(item => item.price > data.min_rent && item.price < data.max_rent);
             }
     
             if(data.bedrooms.length > 0){
-                v = v.filter(item => data.bedrooms.includes( parseInt(item.living_space.split("+")[0])) || ( data.bedrooms.includes( "Studio" ) && item.living_space === "Studio" ) )
+                v = v.filter(item => data.bedrooms.some( (v) => item.living_space.indexOf(v) >= 0 )  )
             }
     
     
             if(data.minsToBus > 0){
                 v = v.filter(item => item.features.minsToBusttop <= data.minsToBus )
+            }
+
+            if(data.rent.length > 0){
+                v = v.filter(item => data.rent.includes(item.features.rents))
+            }
+
+            if(data.area.length > 0){
+                v = v.filter(item => data.area.includes(item.address.area) || data.area.some((v) => item.title.translations.en_us.indexOf(v) >= 0 ) || data.area.some((v) => item.description.translations.en_us.indexOf(v) >= 0 ) || data.area.some((v) => item.address.address_string.indexOf(v) >= 0 ) )
             }
     
             if(data.bathrooms.length > 0){
@@ -103,8 +120,8 @@ exports.getFilteredPlaces = async (req, res) => {
                 }
             }
 
-            if(data.facilities.length > 0){
-                v = v.filter(item => item.features )
+            if( data.facilities.length > 0 ){
+                v = v.filter(item => data.facilities.some( item_2 => item.features[item_2] === true ) )
             }
         }
 
@@ -113,6 +130,10 @@ exports.getFilteredPlaces = async (req, res) => {
 
             if( data.categories.length > 0 ){
                 v = v.filter(item => data.categories.includes(item.category[0].id ))
+            }
+
+            if(data.area > 0){
+                v = v.filter(item => data.area.includes(item.address.area) || data.area.some((v) => item.title.translations.en_us.indexOf(v) >= 0 ) || data.area.some((v) => item.description.translations.en_us.indexOf(v) >= 0 ) || data.area.some((v) => item.address.address_string.indexOf(v) >= 0 ) )
             }
 
             if( data.features.length > 0 ){
@@ -127,13 +148,13 @@ exports.getFilteredPlaces = async (req, res) => {
         
 
 
-        if(data.sortBy === 'Distance'){
+        if(data.sortBy[0] === 'Distance'){
             v = v.sort((a, b) => (a.hitMetadata.distance > b.hitMetadata.distance) ? 1 : -1)
-        }else if(data.sortBy === 'Price'){
-            v = v.sort((a, b) => (a.price > b.price) ? 1 : -1)
-        }else if(data.sortBy === 'Newest'){
-            v = v.sort((a, b) => (a.createdAt < b.createdAt) ? 1 : -1)
-        }else if(data.sortBy === 'Minutes To Busstop'){
+        }else if(data.sortBy[0] === 'Price'){
+            v = v.sort((a, b) => (a.price < b.price) ? 1 : -1)
+        }else if(data.sortBy[0] === 'Newest'){
+            v = v.sort((a, b) => (a.created_at < b.created_at) ? 1 : -1)
+        }else if(data.sortBy[0] === 'Minutes To Busstop'){
             v = v.sort((a, b) => (a.features.minsToBusttop > b.features.minsToBusttop) ? 1 : -1)
         }
 
